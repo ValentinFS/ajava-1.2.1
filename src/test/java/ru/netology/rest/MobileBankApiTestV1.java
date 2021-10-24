fa662d3048914424cb347dccce1845abd8bcdf8e
Valentin Fadeev <valentinfs@yandex.ru>
2021-10-24 22:59:19 +0300

    build.gradle changed

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -11,7 +11,7 @@ class MobileBankApiTestV1 {
         // Given - When - Then
         // Предусловия
         given()
-                .baseUri("http://localhost:9999/api/v1")
+                .baseUri("http://0.0.0.0:9999/api/v1")
                 // Выполняемые действия
                 .when()
                 .get("/demo/accounts")
